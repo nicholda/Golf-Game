94536ad378a1d0deca650ffad55eb22b15ec5ebd
Nicholas Ibbotson <nicholas.ibbotson@outlook.com>
2022-09-09 14:59:18 +0100

    Vector2D

--- ECS/SpriteComponent.h
+++ ECS/SpriteComponent.h
@@ -5,7 +5,7 @@
 
 class SpriteComponent : public Component {
 private:
-	PositionComponent* position;
+	TransformComponent* transform;
 	SDL_Texture* texture;
 	SDL_Rect srcRect, destRect;
 
@@ -20,7 +20,7 @@ public:
 	}
 
 	void init() override {
-		position = &entity->getComponent<PositionComponent>();
+		transform = &entity->getComponent<TransformComponent>();
 
 		srcRect.x = 0;
 		srcRect.y = 0;
